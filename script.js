ee2e1ae10e07a354ab150a9c32c3bb5474a56ff4
shiqi0515 <shiqi0515@outlook.com>
2024-02-02 22:51:21 +0100

    Fix bug in login functionality

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// 假设的商品数据，实际情况下你可能会从服务器获取
+// Fake products data
 const products = {
     beer: [
         { name: "Beer1", price: 39.9 },
@@ -30,7 +30,7 @@ let cart = [];
 
 function filterProducts(category) {
     const productContainer = document.getElementById('product-container');
-    productContainer.innerHTML = ''; // 清空现有内容
+    productContainer.innerHTML = ''; // Clean the container
 
     const categoryProducts = products[category];
     categoryProducts.forEach(product => {
@@ -38,7 +38,7 @@ function filterProducts(category) {
         productElement.className = 'product';
         productElement.innerHTML = `
             <h4>${product.name}</h4>
-            <p>价格: ￥${product.price}</p>
+            <p>Price: SEK ${product.price}</p>
         `;
         productContainer.appendChild(productElement);
     });
@@ -55,13 +55,13 @@ cart.forEach((item, index) => {
     itemElement.className = 'cart-item';
     itemElement.innerHTML = `
         <h4>${item.name}</h4>
-        <p>价格: ￥${item.price}</p>
-        <button onclick="removeFromCart(${index})">移除</button>
+        <p>Price: SEK ${item.price}</p>
+        <button onclick="removeFromCart(${index})">Remove</button>
     `;
     cartItemsContainer.appendChild(itemElement);
 });
 
-totalPriceElement.innerText = total.toFixed(2); // 更新总价
+totalPriceElement.innerText = total.toFixed(2); // Update the total price
 
 
 const categoryProducts = products[category];
@@ -70,8 +70,8 @@ categoryProducts.forEach(product => {
     productElement.className = 'product';
     productElement.innerHTML = `
         <h4>${product.name}</h4>
-        <p>价格: ￥${product.price}</p>
-        <button class="add-to-cart-button" onclick="addToCart(${JSON.stringify(product).replace(/"/g, "&quot;")})">加入购物车</button>
+        <p>Price: SEK ${product.price}</p>
+        <button class="add-to-cart-button" onclick="addToCart(${JSON.stringify(product).replace(/"/g, "&quot;")})">Add to cart</button>
     `;
     productContainer.appendChild(productElement);
 });
